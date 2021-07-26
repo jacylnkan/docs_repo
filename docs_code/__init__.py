2205f0a0aa6397e19a057cda0754a42247f088b1
Jacyln <jacylnkan@gmail.com>
2021-07-26 17:37:13 -0600

    add tags

--- docs_code/__init__.py
+++ docs_code/__init__.py
@@ -1,1 +1,2 @@
-"""Sample documentation written with google-style docstrings. Verified with pydocstyle and darglint."""
+"""Sample documentation written with google-style docstrings. Verified with pydocstyle and darglint. New after adding \
+to CircleCI."""
