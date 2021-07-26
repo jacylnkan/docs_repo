1322058588ebb125a2e0edaa4d40eaca5253bd9b
Jacyln <jacylnkan@gmail.com>
2021-07-26 17:57:03 -0600

    new version

--- docs_code/__init__.py
+++ docs_code/__init__.py
@@ -1,2 +1,1 @@
-"""Sample documentation written with google-style docstrings. Verified with pydocstyle and darglint. New after adding \
-to CircleCI."""
+"""Sample documentation written with google-style docstrings. Verified with pydocstyle and darglint. Version 0.0.2."""
